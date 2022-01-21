5d95e7612578c0a930536b60160b9d4cb67bef09
IhorZviahin <igorzvjagin26@gmail.com>
2022-01-22 00:29:05 +0200

    clw2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Form from "./components/Form/Form";
+import {userService} from "./services/user.service";
+import Users from "./components/Users";
+
+const App = () => {
+    const [users, setUsers] = useState();
+    const [filterusers, setFilterUsers] = useState();
+
+
+    useEffect(() => {
+        userService.getAll().then(value => {
+            setUsers([...value])
+            setFilterUsers([...value])
+        })
+    }, [])
+
+    const getFilter = (data) => {
+        let filterarr = [...users]
+        if(data.name){
+            filterarr = filterarr.filter(user => user.name.toLowerCase().includes(data.name.toLowerCase()))
+        }
+        if(data.username){
+            filterarr = filterarr.filter(user => user.username.toLowerCase().includes(data.username.toLowerCase()))
+        }
+        if(data.email){
+            filterarr = filterarr.filter(user => user.email.toLowerCase().includes(data.email.toLowerCase()))
+        }
+        setFilterUsers(filterarr)
+    }
+
+    return (
+        <div>
+            <Form getFilter={getFilter}/>
+            <Users users ={filterusers}/>
+        </div>
+    );
+};
 
 export default App;
