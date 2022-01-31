5ead626d2de4de13b5c1e5ccd7be826989b2100f
IhorZviahin <igorzvjagin26@gmail.com>
2022-02-01 00:28:21 +0200

    hw 4_1

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ const reduser = (state, action) => {
 
 function App() {
     let [state, dispatch] = useReducer(reduser, {count1: 0});
-
     return (
         <div>
             <div>{state.count1}</div>
