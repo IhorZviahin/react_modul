bb7313f28b0cb3504721de5cc3b7c3e4c21f753b
IhorZviahin <igorzvjagin26@gmail.com>
2022-02-09 22:53:44 +0200

    clw5

--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import {useDispatch} from "react-redux";
+
+import {changeStatus, deletetodo} from "../../store";
+import css from './Todo.module.css'
 
 const Todo = ({todo: {id, name, status}}) => {
+    const dispatch = useDispatch();
     return (
         <div>
-            <input type="checkbox" value={status}/>
-            <div> {id} -- {name} </div>
-            <button>Delate</button>
+            <input type="checkbox" value={status} onChange={() => dispatch(changeStatus({id: id}))}/>
+            <div className={status ? css.info : ''}>
+                {name}
+            </div>
+
+            <button onClick={()=> dispatch(deletetodo({id}))}>Delate</button>
         </div>
     );
 };
--- src/store/todo.slice.js
+++ src/store/todo.slice.js
@@ -9,13 +9,21 @@ const todoSlice = createSlice({
     initialState,
     reducers: {
         addTodo: (state, action) => {
-            state.todos.push({id:new Date().getTime(),name: action.payload.todo, status: false})
+            state.todos.push({id: new Date().getTime(), name: action.payload.todo, status: false})
+        },
+        changeStatus: (state, action) => {
+            const todo = state.todos.find(todo => todo.id === action.payload.id);
+            todo.status = !todo.status
+        },
+        deletetodo:(state, action)=>{
+            const tododelete = state.todos.findIndex(todo => todo.id === action.payload.id);
+            state.todos.splice(tododelete, 1)
         }
     }
 });
 
 const todoReducer = todoSlice.reducer;
 
-export const {addTodo} = todoSlice.actions;
+export const {addTodo, changeStatus,deletetodo} = todoSlice.actions;
 
 export default todoReducer;
